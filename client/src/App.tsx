9f39b6775fb28ac11bbbd8d356605b4e27a3437e
(David) Zak Karachiwala <zak.karachiwala@gmail.com>
2025-12-26 12:42:53 +1100

    Added MUI and Axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,29 @@
+import { List, ListItem, ListItemText } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   
   useEffect(() => {
-      fetch('https://localhost:5001/api/activities')
-        .then(response => response.json())
-        .then(data => setActivities(data));
+      axios.get<Activity[]>('https://localhost:5001/api/activities')
+        .then(response => setActivities(response.data));
 
         return () => {}
     }, []);
 
   return (
-    <div>
-      <h3 className="app" style={{color: 'red'}}>Reactivities</h3>
-      <ul>
+    <>
+      <Typography variant="h3">Reactivities</Typography>
+      <List>
         {activities.map((activity) => (
-          <li key={activity.id}>{activity.title}</li>
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>\
+          </ListItem>
         ))}
-      </ul>
-    </div>
+      </List>
+    </>
   )
 }
 
