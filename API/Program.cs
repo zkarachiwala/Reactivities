21b903c665ea14f99867e651b0b1d0470d936873
(David) Zak Karachiwala <zak.karachiwala@gmail.com>
2025-12-26 12:09:54 +1100

    Added cors

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(policy => policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
---
+++ client/src/lib/types/index.d.ts
@@ -0,0 +1,13 @@
+type Activity = {
+  id: string;
+  title: string;
+  date: string;
+  description: string;
+  category: string;
+  isCancelled: boolean;
+  city: string;
+  venue: string;
+  latitude: number;
+  longitude: number;
+};
+
