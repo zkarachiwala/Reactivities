6a0f1b25039e28165d295b30fe65b546ee13b163
(David) Zak Karachiwala <zak.karachiwala@gmail.com>
2026-01-01 16:39:10 +1100

    Added create key to create route.  End of section 7

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
             { path: '', element: <HomePage />},
             { path: 'activities', element: <ActivityDashboard /> },
             { path: 'activities/:id', element: <ActivityDetail /> },
-            { path: 'createActivity', element: <ActivityForm /> },
+            { path: 'createActivity', element: <ActivityForm key='create' /> },
             { path: 'manage/:id', element: <ActivityForm /> }
 
         ]
