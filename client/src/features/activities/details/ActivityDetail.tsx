f57bda80c584271d77e2262f49321278dfb73e41
(David) Zak Karachiwala <zak.karachiwala@gmail.com>
2026-01-01 16:28:08 +1100

    Fixed Activity detail with router

--- client/src/features/activities/details/ActivityDetail.tsx
+++ client/src/features/activities/details/ActivityDetail.tsx
@@ -1,11 +1,15 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
-import { Link, useNavigate } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
+import { useActivities } from "../../../lib/hooks/useActivities";
 
 export default function ActivityDetail() {
   const navigate = useNavigate();
-  const activity = {} as Activity;  
+  const {id} = useParams();
+  const {activity, isLoadingActivity} = useActivities(id!);
 
-  if (!activity) return <Typography>Loading...</Typography>
+  if (isLoadingActivity) return <Typography>Loading...</Typography>
+
+  if (!activity) return <Typography>Activity not found</Typography>
 
   return (
     <Card sx={{ borderRadius: 4}}>
--- client/src/lib/hooks/useActivities.tsx
+++ client/src/lib/hooks/useActivities.tsx
@@ -1,14 +1,24 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 
-export const useActivities = () => {
+export const useActivities = (id?: string) => {
   const queryClient = useQueryClient();
+
   const {data: activities, isPending} = useQuery({
     queryKey: ['activities'],
     queryFn: async () => {
       const response = await agent.get<Activity[]>('/activities');
       return response.data;
     }
+  });
+
+  const {data: activity, isLoading: isLoadingActivity} = useQuery({
+    queryKey: ['activities', id],
+    queryFn: async () => {
+      const response = await agent.get<Activity>(`/activities/${id}`);
+      return response.data;
+    },
+    enabled: !!id
   });
 
   const updateActivity = useMutation({
@@ -38,5 +48,5 @@ export const useActivities = () => {
     }
   });
 
-  return { activities, isPending, updateActivity, createActivity, deleteActivity };
+  return { activities, isPending, updateActivity, createActivity, deleteActivity, activity, isLoadingActivity };
 }
