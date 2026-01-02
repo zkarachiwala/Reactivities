808b263f85d5430e407f356f67cf315abe3339fa
(David) Zak Karachiwala <zak.karachiwala@gmail.com>
2026-01-02 16:19:49 +1100

    Added computed properties

--- client/src/features/counter/Counter.tsx
+++ client/src/features/counter/Counter.tsx
@@ -1,20 +1,29 @@
-import { Button, ButtonGroup, Typography } from "@mui/material";
+import { Box, Button, ButtonGroup, List, ListItemText, Typography } from "@mui/material";
 import { useStore } from "../../lib/hooks/useStore";
 import { observer } from "mobx-react-lite";
 
 const Counter = observer(function Counter() {
   const { counterStore} = useStore();
   return (
-    <>
-      <Typography variant="h4">{counterStore.title}</Typography>
-      <Typography variant="h5">Count: {counterStore.count}</Typography>
-      <ButtonGroup sx={{mt: 3}}>
-        <Button variant="contained" onClick={() => counterStore.decrement()} color="error">Decrement</Button>
-        <Button variant="contained" onClick={() => counterStore.increment()} color="success">Increment</Button>
-        <Button variant="contained" onClick={() => counterStore.increment(5)} color="primary">Increment by 5</Button>
-      </ButtonGroup>
-    </>
-
+    <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
+      <Box sx={{width: '60%'}}>
+        <Typography variant="h4">{counterStore.title}</Typography>
+        <Typography variant="h5">Count: {counterStore.count}</Typography>
+        <ButtonGroup sx={{mt: 3}}>
+          <Button variant="contained" onClick={() => counterStore.decrement()} color="error">Decrement</Button>
+          <Button variant="contained" onClick={() => counterStore.increment()} color="success">Increment</Button>
+          <Button variant="contained" onClick={() => counterStore.increment(5)} color="primary">Increment by 5</Button>
+        </ButtonGroup>
+      </Box>
+      <Box sx={{width: '40%', p: 4}}>
+        <Typography variant="h6">Event Log (Total: {counterStore.eventCount})</Typography>
+        <List>
+          {counterStore.events.map((event, index) => (
+            <ListItemText key={index}>{index+1}: {event}</ListItemText>
+          ))}
+        </List>
+      </Box>
+    </Box>
   )
 });
 
--- client/src/lib/stores/counterStore.ts
+++ client/src/lib/stores/counterStore.ts
@@ -3,6 +3,9 @@ import { makeAutoObservable } from "mobx";
 export default class CounterStore {
     title = "Counter Store";
     count = 42;
+    events: string[] = [
+        `Initialized with count ${this.count}`
+    ];
 
     constructor() {
         makeAutoObservable(this);
@@ -10,9 +13,15 @@ export default class CounterStore {
 
     increment = (amount =1) => {
         this.count += amount;
+        this.events.push(`Incremented by ${amount} to ${this.count}`);
     }
 
     decrement = (amount =1) => {
         this.count -= amount;
+        this.events.push(`Decremented by ${amount} to ${this.count}`);
+    }
+
+    get eventCount() {
+        return this.events.length;
     }
 }
