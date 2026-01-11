84df78a3f7abfd1a6334500f187c13d4da953571
(David) Zak Karachiwala <zak.karachiwala@gmail.com>
2026-01-11 23:52:53 +1100

    End of section 11

--- client/src/features/activities/details/ActivityDetailsInfo.tsx
+++ client/src/features/activities/details/ActivityDetailsInfo.tsx
@@ -1,12 +1,18 @@
 import { CalendarToday, Info, Place } from "@mui/icons-material";
-import { Divider, Grid2, Paper, Typography } from "@mui/material";
+import { Box, Button, Divider, Grid2, Paper, Typography } from "@mui/material";
 import { formatDate } from "../../../lib/util/util";
+import type { Activity } from "../../../lib/types";
+import { useState } from "react";
+import MapComponent from "../../../app/shared/components/MapComponent";
 
 type Props = {
     activity: Activity;
 }
 
 export default function ActivityDetailsInfo({activity}: Props) {
+
+    const [mapOpen, setMapOpen] = useState(false);
+
     return (
         <Paper sx={{ mb: 2 }}>
 
@@ -33,12 +39,21 @@ export default function ActivityDetailsInfo({activity}: Props) {
                 <Grid2 size={1}>
                     <Place color="info" fontSize="large" />
                 </Grid2>
-                <Grid2 size={11}>
+                <Grid2 size={11} display='flex' justifyContent='space-between' alignItems='center'>
                     <Typography>
                         {activity.venue}, {activity.city}
                     </Typography>
+                    <Button onClick={() => setMapOpen(!mapOpen)}>
+                        {mapOpen ? 'Hide Map' : 'Show map'}
+                    </Button>
+
                 </Grid2>
             </Grid2>
+            {mapOpen && (
+                <Box sx={{height: 400, zIndex: 1000, display: 'block'}}>
+                    <MapComponent position={[activity.latitude, activity.longitude]} venue={activity.venue} />
+                </Box>
+            )}
         </Paper>
     )
 }
