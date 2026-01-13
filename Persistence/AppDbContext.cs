0e3335198f9c7f6f3d5965ee46012edbd9717c17
(David) Zak Karachiwala <zak.karachiwala@gmail.com>
2026-01-13 17:42:01 +1100

    Added identity package

--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -1,9 +1,10 @@
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<User>(options)
 {
     public DbSet<Activity> Activities { get; set; }
 }
