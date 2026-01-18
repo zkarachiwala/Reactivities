e3acba9a432777e29f8819ff2996e9390c79649d
(David) Zak Karachiwala <zak.karachiwala@gmail.com>
2026-01-18 12:12:46 +1100

    Added creator as host attendee to creation method

--- Application/Activities/Commands/CreateActivity.cs
+++ Application/Activities/Commands/CreateActivity.cs
@@ -1,5 +1,6 @@
 using Application.Activities.DTOs;
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using Domain;
 using MediatR;
@@ -14,14 +15,25 @@ public class CreateActivity
         public required CreateActivityDto ActivityDto { get; set; }
     }
 
-    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command, Result<string>>
+    public class Handler(AppDbContext context, IMapper mapper, IUserAccessor userAccessor)
+        : IRequestHandler<Command, Result<string>>
     {
         public async Task<Result<string>> Handle(Command request, CancellationToken cancellationToken)
         {
+            var user = await userAccessor.GetUserAsync();
 
             var activity = mapper.Map<Activity>(request.ActivityDto);
 
             context.Activities.Add(activity);
+
+            var attendee = new ActivityAttendee
+            {
+                ActivityId = activity.Id,
+                UserId = user.Id,
+                IsHost = true
+            };
+
+            context.ActivityAttendees.Add(attendee);
 
             var result = await context.SaveChangesAsync(cancellationToken) > 0;
 
