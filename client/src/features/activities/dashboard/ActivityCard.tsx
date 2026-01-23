8c4245f7b52293b8171295e26506d6c2f6d182ce
(David) Zak Karachiwala <zak.karachiwala@gmail.com>
2026-01-24 09:23:38 +1100

    Updated activity card with attendee data

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -18,11 +18,8 @@ type Props = {
 };
 
 export default function ActivityCard({ activity }: Props) {
-  const isHost = false;
-  const isGoing = false;
-  const label = isHost ? 'You are hosting this activity' : isGoing ? 'You are going to this activity' : '';
-  const isCancelled = false;
-  const color = isHost ? 'secondary' : isGoing ? 'warning' : 'default';
+  const label = activity.isHost ? 'You are hosting this activity' : activity.isGoing ? 'You are going to this activity' : '';
+  const color = activity.isHost ? 'secondary' : activity.isGoing ? 'warning' : 'default';
   
   return (
     <Card elevation={3} sx={{ borderRadius: 3 }}>
@@ -36,13 +33,16 @@ export default function ActivityCard({ activity }: Props) {
           }}}
           subheader={
             <>
-              Hosted by{' '} <Link to={`/profiles/bob`}>Bob</Link>
+              Hosted by{' '} 
+              <Link to={`/profiles/${activity.hostId}`}>
+                {activity.hostDisplayName}
+              </Link>
             </>
           }
         />
         <Box display='flex' flexDirection='column' gap={2} mr={2}>
-          {(isHost || isGoing) && <Chip label={label} color={color} sx={{borderRadius: 2}}/>}
-          {isCancelled && <Chip label='Cancelled' color='error' sx={{borderRadius: 2}}/>}
+          {(activity.isHost || activity.isGoing) && <Chip label={label} color={color} sx={{borderRadius: 2}}/>}
+          {activity.isCancelled && <Chip label='Cancelled' color='error' sx={{borderRadius: 2}}/>}
         </Box>
       </Box>
 
@@ -59,7 +59,15 @@ export default function ActivityCard({ activity }: Props) {
           </Box>
           <Divider />
           <Box display='flex' gap={2} sx={{backgroundColor: 'grey.200', py:3, pl:3}}>
-            Attendees go here
+            {activity.attendees.map(attendee => (
+              <Avatar
+                key={attendee.id}
+                alt={attendee.displayName + ' image'}
+                src={attendee.imageUrl}
+                component={Link}
+                to={`/profiles/${attendee.id}`}
+               />
+            ))}
           </Box>
       </CardContent>
 
